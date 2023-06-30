9c548879169387b0520223f882f032b40802a9bf
João Victor Nascimento <joao.victor@salesoft.com.br>
2023-06-30 09:54:29 -0300

    feat: implementar client para usar builder

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ConsoleClient
+﻿using ConsoleClient.Domain.Builder;
+using Domain;
+
+namespace ConsoleClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = new ProductBuilder();
+
+            ProductDirector.MakeProductToSave(builder);
+
+            var saveProduct = builder.GetProduct();
+            saveProduct.Save(new Product());
+
+            Console.WriteLine("Sucesso ao salvar produto");
         }
     }
 }
