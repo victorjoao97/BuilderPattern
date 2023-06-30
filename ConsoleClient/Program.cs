e5dcee3af89a3ccb20d0138b28dccbdf3f886aa4
João Victor Nascimento <joao.victor@salesoft.com.br>
2023-06-30 10:35:00 -0300

    feat: mostrar mensagens e travar console

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -8,14 +8,25 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iniciando teste de produto");
+            Console.WriteLine("Criando um builder");
             var builder = new ProductBuilder();
 
+            Console.WriteLine("Pedindo ao diretor para que gere um objeto que consiga salvar um produto");
             ProductDirector.MakeProductToSave(builder);
 
             var saveProduct = builder.GetProduct();
             saveProduct.Save(new Product());
-
             Console.WriteLine("Sucesso ao salvar produto");
+
+            Console.WriteLine("Pedindo ao diretor para que gere um objeto que consiga pegar a data atual");
+            ProductDirector.MakeProductToGetDateNow(builder);
+            var productDate = builder.GetProduct();
+            Console.WriteLine($"Data atual: {productDate.DateNow()}");
+
+            Console.WriteLine("Fim do teste de produto");
+            Console.WriteLine("Pressione qualquer tecla para sair");
+            Console.ReadKey();
         }
     }
 }
